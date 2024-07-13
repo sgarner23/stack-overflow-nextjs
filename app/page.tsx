a59ba6e3da2bb170382b39fb5c6ba559f903aa69
stephengarner <stephenallen.garner@gmail.com>
2024-07-13 17:38:21 -0600

    add auth

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">test theme!</h1>
-      <h1 className="h2-bold">test theme!</h1>
-      <h1 className="h3-bold">test theme!</h1>
-    </div>
-  );
-}
